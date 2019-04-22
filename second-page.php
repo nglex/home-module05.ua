43a66fa206a25583b510ef6638ba98f0df5a15f3
Alexander Nagniy <alexandernagniy@gmail.com>
2019-04-22 22:46:55 +0300

    200419

--- second-page.php
+++ second-page.php
@@ -1,81 +1,49 @@
-<?php
-echo 'second page';
-unset($_SESSION['user']);
-unset($_SESSION['lang']);
-session_start();
+<?php session_start(); 
 if(!isset($_SESSION['user'])){
 	header("Location: homework05.php");
 	exit;
-}
+}?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<body>
+<?php
+echo 'second page';
 
-switch ($_SESSION['lang']) {
-	case $_SESSION['lang']=='ru':
-		echo '<br/>Привет, '.$_SESSION['user'].'! :)';
-		break;
-	case $_SESSION['lang']=='en':
-		echo '<br/>Hello, '.$_SESSION['user'].'! :)';
-		break;
-	case $_SESSION['lang']=='ua':
-		echo '<br/>Привіт, '.$_SESSION['user'].'! :)';
-		break;
-	case $_SESSION['lang']=='it':
-		echo '<br/>Ciao, '.$_SESSION['user'].'! :)';
-		break;
-	default:
-		if (!isset($_SESSION['lang'])) {
-
+// switch ($_SESSION['lang']) {
+// 	case $_SESSION['lang']=='ru':
+// 		echo '<br/>Привет, '.$_SESSION['user'].'! :)';
+// 		break;
+// 	case $_SESSION['lang']=='en':
+// 		echo '<br/>Hello, '.$_SESSION['user'].'! :)';
+// 		break;
+// 	case $_SESSION['lang']=='ua':
+// 		echo '<br/>Привіт, '.$_SESSION['user'].'! :)';
+// 		break;
+// 	case $_SESSION['lang']=='it':
+// 		echo '<br/>Ciao, '.$_SESSION['user'].'! :)';
+// 		break;
+// 	}
 ?>
 <form action="second-page.php" method="POST">
 	language: <input type="text" name="lang">
-	<input type="submit" name="lang">
+	<input type="submit" >
 </form>
 <?php
-
-		switch ($_POST['lang']) {
-			case $_POST['lang']=='ru':
-			echo '<br/>Привет, '.$_SESSION['user'].'! :)';	break;
-			case $_POST['lang']=='en':
-				echo '<br/>Hello, '.$_SESSION['user'].'! :)';	break;	
-			case $_SESSION['lang']=='ua':
-				echo '<br/>Привіт, '.$_SESSION['user'].'! :)';	break;
-			case $_SESSION['lang']=='it':
-				echo '<br/>Ciao, '.$_SESSION['user'].'! :)';	break;
-			}
-		}
-		break;
+$trans = [
+'ru'=>'Привет, ',
+'ua'=>'Привіт, ',
+'en'=>'Hello, ',
+'it'=>'Ciao, ',
+];
+if(isset($_POST['lang'])){
+	$_SESSION['lang'] = $_POST['lang'];
 }
+//var_dump($_SESSION);
+echo $trans[$_SESSION['lang']].  ' '. $_SESSION['user'];
 
-
-/*второй вариант*/
-/*if ($_SESSION['lang']=='ru') {
-	echo '<br/>Привет, '.$_SESSION['user'].'! :)';
-}else{
-	if ($_SESSION['lang']=='en') {
-		echo '<br/>Hello, '.$_SESSION['user'].'! :)';
-	}else{
-		if ($_SESSION['lang']=='ua') {
-			echo '<br/>Привіт, '.$_SESSION['user'].'! :)';
-		}else{
-			if ($_SESSION['lang']=='it') {
-				echo '<br/>Ciao, '.$_SESSION['user'].'! :)';
-			}else{
-?>
-<form action="second-page.php" method="POST">
-	language: <input type="text" name="lang">
-	<input type="submit" name="lang">
-</form>
-<?php
-if ($_POST['lang']=='it') {
-	header("Location: second-page.php");
-	echo '<br/>Ciao, '.$_SESSION['user'].'! :)';
-}
-
-				//echo '<br/><a href="homework05.php?logout=true">logout</a>';
-			}
-
-		}
-	}
-}*/
 echo '<br/><a href="homework05.php?logout=true">logout</a>';
-
 ?>
+</body>
+</html> 
